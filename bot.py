6f4c9553eab0a645526e6a129ef3589f1c72566c
sridevipanuganti180-spec <sridevipanuganti180@gmail.com>
2026-09-07 10:39:28 +0530

    Refactor bot.py to improve structure and readability

--- bot.py
+++ bot.py
@@ -91,8 +91,8 @@ def start(message):
         message,
         "Welcome! 🎬\nSend me a video URL."
     )
- @bot.callback_query_handler(func=lambda call: call.data == "check_join")
- def check_join(call):
+@bot.callback_query_handler(func=lambda call: call.data == "check_join")
+def check_join(call):
 
     if is_user_joined(call.from_user.id):
         bot.answer_callback_query(
